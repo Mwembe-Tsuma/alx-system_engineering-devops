96ee4e6c1466027209c176d132f5b06845d2fba9
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-11-07 12:51:54 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+
+            subscribers = data['data']['subscribers']
+
+            return subscribers
+        except (KeyError, ValueError):
+            return 0
+    else:
+        return 0
