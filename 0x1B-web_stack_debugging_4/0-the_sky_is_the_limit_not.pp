fcb1fc366b08dca2e7f19f729998c6075a821d27
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-11-15 07:56:50 +0000

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increase the ULIMIT of the default file
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Exec['nginx-restart'], # Notify the nginx-restart
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+  refreshonly => true,
+}
