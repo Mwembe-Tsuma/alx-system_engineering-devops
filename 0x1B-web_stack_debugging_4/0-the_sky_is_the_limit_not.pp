53f28a280cadefd87ae4a67c86fbcdb179934434
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-11-16 09:05:34 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # Increase the ULIMIT of the default file
 
 # fixing for nginx
-exec { 'fix-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/',
+  path    => '/etc/init.d/'
 }
